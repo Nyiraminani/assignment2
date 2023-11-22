bcfa6ad19f247451d808a4096d172f69b24539aa
Nyiraminani <151008047+Nyiraminani@users.noreply.github.com>
2023-11-22 19:02:47 +0200

    Add files via upload

---
+++ interface1.java
@@ -0,0 +1,14 @@
+class interface1 {  
+public static void main(String args[]){  
+Drawable d=new Rectangle();  
+d.draw();  
+System.out.println(Drawable.cube(3));  
+}}  
+interface Drawable{  
+void draw();  
+static int cube(int x){return x*x*x;}  
+}  
+class Rectangle implements Drawable{  
+public void draw(){System.out.println("drawing rectangle");}  
+}  
+  
